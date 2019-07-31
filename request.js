57335c01ef8d77cdbcd3e8902ed87008e297f660
fengshangshi <fss123@gmail.com>
2019-07-31 18:14:05 +0800

    Update request.js

--- request.js
+++ request.js
@@ -1,1 +1,75 @@
 
+class Request {
+  static unique = new Request();
+  constructor() {
+    // 单例
+    if (Request.unique) {
+      return Request.unique
+    }
+   this.running_count = 0;
+   this.await_queue = [];
+   this.max_limit = 10;
+  }
+  async _fetch(uri, options) {
+    const defaultOptions = {
+      ...options,
+      headers: {
+        ...options.headers,
+      }
+    }
+    if (this.running_count === this.max_limit) {
+       await this._wait();
+    }
+    return new Promise((resolve, reject) => {
+      this.running_count += 1;
+      fetch(uri, options).then(response => {
+        const isJsonData = response.headers.get('content-type').indexOf('application/json') > -1;
+        if (isJsonData) {
+          response.json()
+            .then(resolve)
+            .catch(reject)
+        } else {
+          response.text()
+            .then(resolve)
+            .catch(reject)
+        }
+      }).catch((err) => {
+        reject(err);
+      }).finally(() => {
+        this.running_count -= 1;
+        this._letsGo();
+      })
+    })
+  }
+  _wait(){
+    return new Promise((res) => {
+      this.await_queue.push(res);
+    })
+  }
+  _letsGo(){
+    if(this.await_queue.length === 0){
+      return
+    } 
+    this.await_queue.shift()();
+  }
+  get(uri) {
+    return this._fetch(uri, {
+      method: 'get'
+    })
+  }
+  post(uri) {
+    return this._fetch(uri, {
+      method: 'post'
+    })
+  }
+}
+
+// test
+
+const request = new Request();
+
+Array(25).fill('https://www.baidu.com').forEach((uri,index) => {
+  request.get(uri).then(()=> {
+    console.info('im ok',index)
+  })
+});
